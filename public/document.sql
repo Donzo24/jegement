bff264331ec541cb1d664fc8798d5f04f7b51037
Youssouf Donzo <donzoyoussouf@gmail.com>
2021-02-03 14:34:46 +0000

    Update document.sql

--- public/document.sql
+++ public/document.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mer. 03 fév. 2021 à 14:27
+-- Généré le : mer. 03 fév. 2021 à 14:34
 -- Version du serveur :  5.7.32-0ubuntu0.18.04.1
 -- Version de PHP : 7.4.12
 
@@ -22,48 +22,12 @@ SET time_zone = "+00:00";
 -- Base de données : `casier`
 --
 
--- --------------------------------------------------------
-
---
--- Structure de la table `document`
---
-
-CREATE TABLE `document` (
-  `id_document` int(11) NOT NULL,
-  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `template` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `variables` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `datas` longtext COLLATE utf8mb4_unicode_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Déchargement des données de la table `document`
 --
 
 INSERT INTO `document` (`id_document`, `nom`, `template`, `slug`, `variables`, `datas`) VALUES
 (1, 'Jugement supletif', 'public/templates/jugement-supletif-template.bin', 'jugement-supletif', 'date_jour:Date du jour\r\netat_civil:Etat civil\r\ndate_requete: Date de requete\r\nsexe_requerant:Civilite du requerant\r\nrequerant:Prenom et Nom/Profession/Adresse\r\nsexe: Civilite (Mr/Mme)\r\nnom:Nom\r\nprenom:Prenom\r\ndate_naissance:Date de naissance\r\nlieu_naissance:Lieu de naissance\r\npere:Prenon du pere\r\nmere:Prenom et nom de la mere\r\ntemoin_1: Temoin 1 Mr/MMe Nom, Prenom, Profession, Adresse\r\ntemoin_2: Temoin 2 Mr/Mme Nom, Prenom, Profession, Adresse', 'etat_civil:Matam-Conakry\r\ntribunal:CONAKRY III- MAFANCO\r\npremier_signataire:Youssouf\r\nsecond_secondaire:Donzo\r\nparticipant_1:Madame  Lamarana DIALLO\r\nparticipant_2:Madame  Josephine  Loly TINKIANO\r\nparticipant_3:Monsieur  Lonceny  KANDE');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `document`
---
-ALTER TABLE `document`
-  ADD PRIMARY KEY (`id_document`),
-  ADD UNIQUE KEY `document_nom_unique` (`nom`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `document`
---
-ALTER TABLE `document`
-  MODIFY `id_document` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
